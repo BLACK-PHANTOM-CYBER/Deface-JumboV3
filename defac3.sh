8cc359a54558448f01e07e87e5d4b1806b95741f
BLACK PHANTOM CYBER <46845458+BLACK-PHANTOM-CYBER@users.noreply.github.com>
2019-02-08 15:50:35 +0700

    Add files via upload

---
+++ defac3.sh
@@ -0,0 +1,52 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+sleep 1
+toilet -f standard "1" | lolcat
+sleep 3
+clear
+echo
+sleep 1
+toilet -f standard "2" | lolcat
+sleep 3
+clear
+echo
+sleep 1
+toilet -f standard "3" | lolcat
+sleep 3
+clear
+echo
+sleep 1
+toilet -f standard "4" | lolcat
+sleep 3
+clear
+echo
+sleep 1
+toilet -f standard "5" | lolcat
+sleep 3
+clear
+toilet -f slant "JUMBO V3" | lolcat
+echo
+echo
+echo "==============================" | lolcat
+echo $red"CREATED BY MR.SPOON"
+echo $green"TEAM BLACK PHANTOM CYBER"
+echo $blue"JUMLAH WEB YG BISA DI DEFACE 200"
+echo $puprle"VERSION TOOL 3 (FINAL)"
+echo "==============================" | lolcat
+echo
+echo
+echo "MAU LANJUT y===>"
+read y 
+read t
+
+if [ $y ]; then
+ echo
+ sh mechan.sh
+
+fi
